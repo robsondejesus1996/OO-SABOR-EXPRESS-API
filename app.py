ce8445422a757341a7a1322d3ccd8a5ce5ca1a7f
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-04-18 15:15:39 -0300

    Atualização de herança

--- app.py
+++ app.py
@@ -6,11 +6,17 @@ restaurante_praca = Restaurante('praça', 'Gourmet')
 bebibda_suco = Bebida('Suco de Melancia', 5.0 , 'grande')
 prato_paozinho = Prato('Paozinho', 2.00, 'O melhor pão da cidade')
 
+bebibda_suco.aplicar_desconto()
+prato_paozinho.aplicar_desconto()
+
+restaurante_praca.adicionar_no_cardapio(bebibda_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
+
+
 
 
 def main():
-    print(bebibda_suco)
-    print(prato_paozinho)
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
--- modelos/cardapio/prato.py
+++ modelos/cardapio/prato.py
@@ -6,5 +6,8 @@ class Prato(ItemCardapio):
         super().__init__(nome, preco)
         self.descricao = descricao
 
-    def __str__(self)        :
+    def __str__(self):
         return self._nome
+    
+    def aplicar_desconto(self):
+        self._preco -= (self._preco * 0.05)
